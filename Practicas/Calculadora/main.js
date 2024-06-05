14d879c1a5c1986de07133d045953f4d558332ed
KarlaSofia21 <sofiadrew432@gmail.com>
2024-06-04 22:32:31 -0600

    Add files via upload

---
+++ Practicas/Calculadora/main.js
@@ -0,0 +1,80 @@
+function Sumar(){
+    var valor1= parseFloat(document.getElementById("v1").value)
+    var valor2= parseFloat(document.getElementById("v2").value)
+    var h3=document.getElementById("res")
+    
+    if(isNaN(valor1) || isNaN(valor2)){
+        if(isNaN(valor1)){
+            h3.innerHTML="El valor 1 no es valido"
+            document.getElementById("v1").focus()
+        }else{
+            h3.innerHTML="El valor 2 no es valido"
+            document.getElementById("v2").focus()
+        }
+        return
+    }
+
+    var resultado=valor1+valor2
+    
+    h3.innerHTML=resultado
+}
+function Restar(){
+    var valor1= parseFloat(document.getElementById("v1").value)
+    var valor2= parseFloat(document.getElementById("v2").value)
+    var h3=document.getElementById("res")
+    
+    if(isNaN(valor1) || isNaN(valor2)){
+        if(isNaN(valor1)){
+            h3.innerHTML="El valor 1 no es valido"
+            document.getElementById("v1").focus()
+        }else{
+            h3.innerHTML="El valor 2 no es valido"
+            document.getElementById("v2").focus()
+        }
+        return
+    }
+
+    var resultado=valor1-valor2
+    
+    h3.innerHTML=resultado
+}
+function Multiplicar(){
+    var valor1= parseFloat(document.getElementById("v1").value)
+    var valor2= parseFloat(document.getElementById("v2").value)
+    var h3=document.getElementById("res")
+    
+    if(isNaN(valor1) || isNaN(valor2)){
+        if(isNaN(valor1)){
+            h3.innerHTML="El valor 1 no es valido"
+            document.getElementById("v1").focus()
+        }else{
+            h3.innerHTML="El valor 2 no es valido"
+            document.getElementById("v2").focus()
+        }
+        return
+    }
+
+    var resultado=valor1*valor2
+    
+    h3.innerHTML=resultado
+}
+function Dividir(){
+    var valor1= parseFloat(document.getElementById("v1").value)
+    var valor2= parseFloat(document.getElementById("v2").value)
+    var h3=document.getElementById("res")
+    
+    if(isNaN(valor1) || isNaN(valor2)){
+        if(isNaN(valor1)){
+            h3.innerHTML="El valor 1 no es valido"
+            document.getElementById("v1").focus()
+        }else{
+            h3.innerHTML="El valor 2 no es valido"
+            document.getElementById("v2").focus()
+        }
+        return
+    }
+
+    var resultado=valor1/valor2
+    
+    h3.innerHTML=resultado
+}
